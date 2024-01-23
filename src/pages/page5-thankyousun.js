ec4c993768f06bfd020bdb443ad311c1488dcf4b
Antonio Rodriguez <antoniwan@icloud.com>
2024-01-23 12:12:29 -0400

    Page 5 translation

--- src/pages/page5-thankyousun.js
+++ src/pages/page5-thankyousun.js
@@ -2,17 +2,35 @@ import PageWrapper from "../components/PageWrapper";
 import styles from "./ContentPage.module.css";
 import graphic from "../graphics/page-4.png";
 
-export default function Page5() {
+export default function Page5({ language }) {
   return (
     <PageWrapper>
       <div className={styles.columnsWrapper}>
         <div className={` ${styles.textArea}`}>
-          <p className={styles.paragraph}>
-            As another day goes by, and whenever Mia feels the sunshine on her
-            skin, her smile shines, and as she looks into the sky with a bright,
-            big smile, she screams,{" "}
-            <em>"Hi, Sun! Thank you for the warmth and light!"</em>
-          </p>
+          {language === "en" && (
+            <>
+              <p className={styles.paragraph}>
+                As another day goes by, and whenever Mia feels the sunshine on
+                her skin, her smile shines, and as she looks into the sky with a
+                bright, big smile, she screams,{" "}
+                <em>"Hi, Sun! Thank you for the warmth and light!"</em>
+              </p>
+            </>
+          )}
+
+          {language === "es" && (
+            <>
+              <p className={styles.paragraph}>
+                Hoy es otro día nuevo, y cada vez que Mia sienta la luz del sol
+                en su piel, su sonrisa brilla y mirando al cielo, con su gran y
+                brillante sonrisa, grita:{" "}
+                <em>
+                  “¡Hola, Sol! ¡Gracias por el calorcito y por la luz! ¡Gracias
+                  por un nuevo día de diversión y juegos!
+                </em>
+              </p>
+            </>
+          )}
         </div>
 
         <div className={` ${styles.graphicColumn}`}>
