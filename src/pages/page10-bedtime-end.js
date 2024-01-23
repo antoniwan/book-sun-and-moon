7c7d1e77e7f39cd100211a4eaff8eab7d30fd0ab
Antonio Rodriguez <antoniwan@icloud.com>
2024-01-23 12:36:27 -0400

    Page 10 translations complete - content translations complete

--- src/pages/page10-bedtime-end.js
+++ src/pages/page10-bedtime-end.js
@@ -2,20 +2,37 @@ import PageWrapper from "../components/PageWrapper";
 import styles from "./ContentPage.module.css";
 import welcomeGraphic from "../graphics/graphic-goodnight.png";
 
-export default function Page10() {
+export default function Page10({ language }) {
   return (
     <PageWrapper>
       <div
         className={`${styles.columnsWrapper} ${styles.columnWrapperReverse}`}
       >
         <div className={` ${styles.textArea}`}>
-          <p className={styles.paragraph}>
-            <em>"It's time for bed,"</em> said Mia with the moonshine glowing on
-            her face, and so happy that tomorrow is another day.{" "}
-            <em>"Good night, moon!"</em>
-          </p>
+          {language === "en" && (
+            <>
+              <p className={styles.paragraph}>
+                <em>"It's time for bed,"</em> said Mia with the moonshine
+                glowing on her face, and so happy that tomorrow is another day.{" "}
+                <em>"Good night, moon!"</em>
+              </p>
 
-          <h3 className={styles.theEnd}>The End</h3>
+              <h3 className={styles.theEnd}>The End</h3>
+            </>
+          )}
+
+          {language === "es" && (
+            <>
+              <p className={styles.paragraph}>
+                <em>“Es hora de dormir”</em>, dijo Mia con la luz de la luna
+                tocándole la carita, y feliz de que mañana será otro día y verá
+                al sol.
+                <em>“¡Buenas noches, Luna!”</em>
+              </p>
+
+              <h3 className={styles.theEnd}>Fin</h3>
+            </>
+          )}
         </div>
 
         <div className={` ${styles.graphicColumn}`}>
