1e85acaabd4416d90066e0ed28ae0a2bae782704
Antonio Rodriguez <antoniwan@icloud.com>
2024-01-23 12:15:26 -0400

    Page 6 translations

--- src/pages/page6-shadowplay.js
+++ src/pages/page6-shadowplay.js
@@ -2,19 +2,36 @@ import PageWrapper from "../components/PageWrapper";
 import styles from "./ContentPage.module.css";
 import graphic from "../graphics/graphic-shadowplay.png";
 
-export default function Page6() {
+export default function Page6({ language }) {
   return (
     <PageWrapper>
       <div
         className={`${styles.columnsWrapper} ${styles.columnWrapperReverse}`}
       >
         <div className={` ${styles.textArea}`}>
-          <p className={styles.paragraph}>
-            As the hours of each day go by and the sun passes, it creates many
-            shadows for our brave and joyful girl and her friends to play and
-            enjoy. <em>"The sun also plays with us!"</em> Mia explains
-            excitedly.
-          </p>
+          {language === "en" && (
+            <>
+              <p className={styles.paragraph}>
+                As the hours of each day go by and the sun passes, it creates
+                many shadows for our brave and joyful girl and her friends to
+                play and enjoy. <em>"The sun also plays with us!"</em> Mia
+                explains excitedly.
+              </p>
+            </>
+          )}
+
+          {language === "es" && (
+            <>
+              <p className={styles.paragraph}>
+                Las horas de cada día pasan y el sol a su vez va pasando y
+                moviéndose, creando muchas sombras para que nuestra niña
+                valiente y alegre y sus amigos disfruten. Ellos inventan juegos
+                con las sombras y su oscuridad.{" "}
+                <em>“El sol también quiere jugar con nosotros”</em> Mia le
+                explica a sus amigos.
+              </p>
+            </>
+          )}
         </div>
 
         <div className={` ${styles.graphicColumn}`}>
